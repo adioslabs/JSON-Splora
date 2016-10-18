081826fe10252fb7383df48a34664b43ed7219a6
Wells Johnston <wells@Wellss-MBP.fios-router.home>
2016-10-17 22:53:11 -0400

    made some good progress on parsing and displaying json objects

--- js/main.js
+++ js/main.js
@@ -1,27 +1,86 @@
 'use strict'
 
-const JSONObject = require('./json');
+const prettyJSON = require('./pretty-json');
+const json2HTML = require('node-json2html');
+const json5 = require('json5');
 const $ = require('jquery');
 const fs = require('fs');
 
-let mainWindow = $('main');
+class App {
 
-$('textarea').on('focus', _ => {
-  $('textarea').val('');
-});
+  constructor(document) {
 
-document.ondragover = document.ondrop = (ev) => {
-  ev.preventDefault();
-};
+    this.outputContainer = $('#output-container');
+    this.messageWindow = $('#message-window');
+    this.textArea = $('#main textarea');
+    this.queryArea = $('#query');
+    this.document = document;
 
-document.body.ondrop = (ev) => {
-  ev.preventDefault();
-  let path = ev.dataTransfer.files[0].path;
-  let text = fs.readFileSync(path).toString();
-  let el = $('#main');
-  new JSONObject(text, el);
-  JSONObject.on('err', message => {
-    console.log('here');
-    $('#message-window').text(message);
-  });
-};
+    // initial update
+    this.update(this.textArea.val());
+
+    /**
+     * Drag / drop
+     */
+
+    document.ondragover = document.ondrop = (ev) => {
+      ev.preventDefault();
+    };
+
+    document.body.ondrop = (ev) => {
+      ev.preventDefault();
+      let path = ev.dataTransfer.files[0].path;
+      let text = fs.readFileSync(path).toString();
+      let el = $('#main');
+      this.update(text);
+    };
+
+    this.textArea.on('input', i => {
+      this.textArea.val(this.textArea.val().trim());
+      this.input = this.textArea.val();
+      this.update();
+    });
+  }
+
+  update(text) {
+    try {
+      this.data = json5.parse(this.input);
+      this.clearMessage();
+      this.show();
+      this.textArea.hide();
+      this.queryArea.show();
+    } catch (e) {
+      this.message(e.message);
+    }
+  }
+
+  message(message) {
+    this.messageWindow.text(message);
+  }
+
+  show() {
+    this.outputContainer.append('<div id="output"></div>');
+    this.output = $('#output');
+    this.view = new prettyJSON.view.Node({
+      data: this.data,
+      el: this.output
+    })
+  }
+
+  clearOutput() {
+    this.output.remove();
+  }
+
+  clearMessage() {
+    this.messageWindow.text('');
+  }
+
+}
+
+const app = new App(document);
+
+/**
+ * Export app
+ */
+
+module.exports = app;
